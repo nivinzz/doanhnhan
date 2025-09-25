46914ed24f0d04313e7ffff1ec4fa3384426960e
nivinzz <128696218+nivinzz@users.noreply.github.com>
2025-09-25 14:52:52 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
+// Lấy root element
+const rootElement = document.getElementById("root");
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("Root element not found");
 }
 
+// Render App
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
